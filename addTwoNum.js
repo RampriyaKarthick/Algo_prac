3f5c5953c24e0711a4fd20b344201ac52f8f4d0b
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-05-06 14:26:15 +0200

    addTwoNum.js

--- addTwoNum.js
+++ addTwoNum.js
@@ -1,1 +1,18 @@
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+function sumTwoNum(nums, target){
+    // create new map
+    const myMap = new Map();
+    for(let i =0; i<nums.length; i++){
+        let targetDifference = target -nums[i];
+        if(myMap.has(targetDifference)){
+            return [myMap.get(targetDifference), i]
+        }
+        myMap.set(nums[i] , i)
+    }
+    return null;
+}
+
+let firstArray = sumTwoNum([8,4,6,1,3,2,-6,5], 13)
+console.log(firstArray)
